085ac7a19c5c3dfb660b9bff7491427fb94c75dd
Fabiano Riccardi <fabiano.riccardi@outlook.com>
2020-08-06 09:14:40 +0200

    print "duplicates" products

--- graveyard.test.js
+++ graveyard.test.js
@@ -8,6 +8,34 @@ slugify.extend({
   '@': 'at',
 });
 
+
+const compareItems = (a, b) => {
+  if (a.slug > b.slug) {
+    return 1;
+  } else if (a.slug < b.slug) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
+}
+
+const findSlugDuplicates = (arr) => {
+  // You can define the comparing function here.
+  let sorted_arr = arr.slice().sort(compareItems);
+
+  // JS by default uses a crappy string compare.
+  // (we use slice to clone the array so the
+  // original array won't be modified)
+  let results = [];
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1].slug == sorted_arr[i].slug) {
+      results.push(sorted_arr[i].slug);
+    }
+  }
+  return results;
+}
+
 describe('graveyard', () => {
   it('objects should be valid', () => {
     data.forEach((product) => {
@@ -58,6 +86,9 @@ describe('graveyard', () => {
     });
     // Create a set (removes any duplicate slugs)
     const items = [...new Set(data.map(item => item.slug))];
+
+    console.log("Duplicates found: ");
+    console.log(findSlugDuplicates(data));
 
     // Both the data and items arr should have the same length
     expect(items.length).toBe(data.length);
